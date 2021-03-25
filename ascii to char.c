4032aa248510d2f0853e3371cdedabe41a1ff2b1
Simran Joon <78432689+simranjoon@users.noreply.github.com>
2021-03-25 12:30:57 +0530

    Create ascii to char.c

---
+++ ascii to char.c
@@ -0,0 +1,10 @@
+//Program to print the character of given ASCII value
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a ASCII value(0 to 255)\n");
+	scanf("%d",&num); 	
+	printf("Equivalent Character: %c",num);	
+	return  0;
+}
