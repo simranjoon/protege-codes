6024eddfa09609f9af3d42044f9608ecfe83d0b7
Simran Joon <78432689+simranjoon@users.noreply.github.com>
2021-04-10 19:02:54 +0530

    Create full * pyramid.c

---
+++ full * pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+   int i, space, n, k = 0;
+   printf("Enter the number of rows: ");
+   scanf("%d", &n);
+   for (i = 1; i <= n; ++i, k = 0) {
+      for (space = 1; space <= n - i; ++space) {
+         printf("  ");
+      }
+      while (k != 2 * i - 1) {
+         printf("* ");
+         ++k;
+      }
+      printf("\n");
+   }
+   return 0;
+}
